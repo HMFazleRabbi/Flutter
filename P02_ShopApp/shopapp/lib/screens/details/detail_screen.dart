225f1904b855a6a8f6479ed31d3909ca834b1b2c
fazle01 <h-m-fazle.rabbi@vitrox.com>
2020-10-24 19:07:13 +0800

    Added color dot in the detail page.

--- P02_ShopApp/shopapp/lib/screens/details/detail_screen.dart
+++ P02_ShopApp/shopapp/lib/screens/details/detail_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shopapp/constants.dart';
 import 'package:shopapp/models/Product.dart';
 import 'package:shopapp/screens/home/home_screen.dart';
+import 'package:shopapp/screens/details/component/body.dart';
 
 class DetailScreen extends StatelessWidget {
   final Product product;
@@ -12,28 +13,34 @@ class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: product.color,
-        elevation: 0,
-        leading: IconButton(
-          icon: SvgPicture.asset('assets/icons/back.svg'),
-          onPressed: () => Navigator.push(
-              context, MaterialPageRoute(builder: (context) => HomeScreen())),
-        ),
-        actions: [
-          IconButton(
-          icon: SvgPicture.asset('assets/icons/search.svg'),
-          onPressed: () => Navigator.push(
-              context, MaterialPageRoute(builder: (context) => HomeScreen())),
-        ),
-        IconButton(
-          icon: SvgPicture.asset('assets/icons/add_to_cart.svg'),
-          onPressed: () => Navigator.push(
-              context, MaterialPageRoute(builder: (context) => HomeScreen())),
-        ),
-        SizedBox(width: kDefaultPadding/2)
-        ],
+      backgroundColor: product.color,
+      appBar: buildAppBar(context),
+      body: Body(product: product),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: product.color,
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset('assets/icons/back.svg'),
+        onPressed: () => Navigator.push(
+            context, MaterialPageRoute(builder: (context) => HomeScreen())),
       ),
+      actions: [
+        IconButton(
+        icon: SvgPicture.asset('assets/icons/search.svg'),
+        onPressed: () => Navigator.push(
+            context, MaterialPageRoute(builder: (context) => HomeScreen())),
+      ),
+      IconButton(
+        icon: SvgPicture.asset('assets/icons/add_to_cart.svg'),
+        onPressed: () => Navigator.push(
+            context, MaterialPageRoute(builder: (context) => HomeScreen())),
+      ),
+      SizedBox(width: kDefaultPadding/2)
+      ],
     );
   }
 }
